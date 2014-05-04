ca38274eea49098e184f621acb251ceb65f30591
Krzysiek <krz.gdk@gmail.com>
2014-05-04 22:21:18 +0200

    added graph generation on the fly (gets slow on 2000 values)

--- Main/Main.java
+++ Main/Main.java
@@ -1,6 +1,13 @@
 package Main;
 
+import java.awt.*;
 import java.util.ArrayList;
+import org.jfree.chart.*;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
 
 public class Main {
 
@@ -14,7 +21,15 @@ public class Main {
         ArrayList<Schedule> temporary;
 
         int iteration_number = 0;
-               
+        XYSeries series = new XYSeries("XYGraph");
+
+
+        JFrame f = new JFrame();
+        f.setSize(640,480);
+        f.setVisible(true);
+
+        ChartPanel chPanel = new ChartPanel(null); //creating the chart panel, which extends JPanel
+        chPanel.setPreferredSize(new Dimension(640, 480)); //size according to my window
         do{
             Genetic.rank(population);
             Genetic.scaleRank(population); 
@@ -34,18 +49,41 @@ public class Main {
             for(Schedule s : population)
                 if(local_best.rate < s.rate)
                     local_best = s;
-            System.out.print("" + iteration_number + ": " + local_best.rate + "\n");
+            //System.out.print("" + iteration_number + ": " + local_best.rate + "\n");
             /**************************************/
-            
+
+
+            series.add(iteration_number,local_best.rate);
+
+            if(iteration_number % 50 == 0) {
+                XYSeriesCollection dataset = new XYSeriesCollection();
+                dataset.addSeries(series);
+                // Generate the graph
+                JFreeChart chart = ChartFactory.createXYLineChart(
+                        "Adaptation graph", // Title
+                        "Generation", // x-axis Label
+                        "Rating", // y-axis Label
+                        dataset, // Dataset
+                        PlotOrientation.VERTICAL, // Plot Orientation
+                        true, // Show Legend
+                        true, // Use tooltips
+                        false // Configure chart to generate URLs?
+                );
+
+                chPanel.setChart(chart);
+                chPanel.setMouseWheelEnabled(true);
+                f.add(chPanel);
+                f.validate();
+            }
             iteration_number++;
         }while(true);
-        
+
         Schedule local_best = population.get(0);
         for(Schedule s : population)
             if(local_best.rate < s.rate)
                local_best = s;
         
         System.out.print(local_best.toString() + local_best.rate + "\n");
-    }            
-    
+    }
+
 }
