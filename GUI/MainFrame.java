b774a0666beae15264b5993d70a37d1da2b2e33a
Krzysiek <krz.gdk@gmail.com>
2014-05-14 17:46:09 +0200

    no sigma scaling

--- GUI/MainFrame.java
+++ GUI/MainFrame.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package GUI;
 
 import Main.Genetic;
 import Main.Main;
 
-/**
- *
- * @author Mariusz
- */
+
 public class MainFrame extends javax.swing.JFrame {
 
     protected Thread program;
